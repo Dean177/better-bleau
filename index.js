1954351526b13e4137a5cede174d5f065474c666
Dean Merchant <dean.merchant@kairostech.io>
2020-09-12 00:10:39 +0200

    afesfaesfadsafds

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
 const fs = require('fs')
-var swat_proxy = require('swat-proxy');
+const { promisify } = require('util')
+const readFile = promisify(fs.readFile)
 
-fs.readFile('./app.js', (error, jsFile) => {
-    if (error) {
-        throw error
-    }
-    fs.readFile('./app.js', (error, cssFile) => {
-        if (error) {
-            throw error
-        }
-        // Add some JS to the end of the Google homepage.
-        swat_proxy.proxy('https://bleau.info/', {
-            selector: 'body',
-            manipulation: swat_proxy.Manipulations.APPEND,
-            matchType: swat_proxy.MatchTypes.DOMAIN,
-            content: `
-                <style>${cssFile}</style>
-                <script>${jsFile}</script>
-            `
-        });
+const loadAssets = Promise.all([readFile('./app.js'), readFile('./app.css')])
 
-        swat_proxy.start();
-    })
+let content = null
+
+const app = express()
+
+app.use('*', async (request, response, next) => {
+  const url = `https://bleau.info${request.originalUrl}`
+  const bleauResponse = await axios.get(url)
+  if (bleauResponse.headers['content-type']?.includes('text/html')) {
+    const $ = cheerio.load(bleauResponse.data)
+    $('body').append(content)
+    response.send($.html())
+  } else {
+    response.redirect(url)
+  }
 })
 
+loadAssets.then(([jsFile, cssFile]) => {
+  content = `
+    <style>${cssFile}</style>
+    <script>${jsFile}</script>
+  `
+  app.listen(process.env.PORT || 3000, () => {
+    console.info('Started')
+  })
+})
